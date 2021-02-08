916a0ca63a849f42284240dc6c8308eb0192723b
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-08 13:01:08 +0530

    remove outputDir from vue configuration

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 const apiUrl = process.env.VUE_APP_API_URL || `http://localhost:3000`;
 
 module.exports = {
-	outputDir: path.resolve(__dirname, "../server/public"),
 	devServer: {
 		disableHostCheck: process.env.IS_GITPOD ? true : false,
 		proxy: {
