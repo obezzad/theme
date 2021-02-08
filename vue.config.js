c5d20dd786216986e5cdc5a1b74e26bac70b2903
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-08 13:01:48 +0530

    proxy '/api' to VUE_APP_SERVER_URL or localhost

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
-const path = require("path");
-
-const apiUrl = process.env.VUE_APP_API_URL || `http://localhost:3000`;
+const serverUrl = process.env.VUE_APP_SERVER_URL || `http://localhost:3000`;
 
 module.exports = {
 	devServer: {
 		disableHostCheck: process.env.IS_GITPOD ? true : false,
 		proxy: {
 			"/api": {
-				target: apiUrl
+				target: serverUrl
 			}
 		}
 	},
