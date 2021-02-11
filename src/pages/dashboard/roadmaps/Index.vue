3ded5bb4f0a1eb8bc2bc6e572fc3e90dcf08127f
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-11 10:11:14 +0530

    fix: route to roadmap settings after createRoadmap

--- src/pages/dashboard/roadmaps/Index.vue
+++ src/pages/dashboard/roadmaps/Index.vue
@@ -119,7 +119,7 @@ export default {
 				const response = await createRoadmap();
 
 				const url = response.data.roadmap.url;
-				this.$router.push(`/dashboard/roadmap/${url}/settings`);
+				this.$router.push(`/dashboard/roadmaps/${url}/settings`);
 			} catch (err) {
 				console.error(err);
 			}
