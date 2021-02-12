c4b2af1ebe37e8b937e6d6046afbc7343c15f483
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-12 20:49:48 +0530

    create bare layout
    Bare layout only contain site logo

---
+++ src/layout/Bare.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="layout-bare">
+		<header>
+			<router-link to="/" class="site-info">
+				<img
+					class="site-logo"
+					:src="getSiteSittings.logo"
+					:alt="getSiteSittings.title"
+				>
+				<h5 class="site-name">
+					{{ getSiteSittings.title }}
+				</h5>
+			</router-link>
+		</header>
+		<router-view />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BareLayout",
+	computed: {
+		getSiteSittings() {
+			return this.$store.getters["settings/get"];
+		}
+	}
+};
+</script>
