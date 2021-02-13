4470392323fb15d184a8eebc0038033e435255fa
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-13 10:34:22 +0530

    remove vue-moment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueMeta from "vue-meta";
-import moment from "vue-moment";
 
 import App from "./App.vue";
 import router from "./routes";
@@ -12,7 +11,6 @@ import "@/styles/main.sass";
 Vue.config.productionTip = false;
 
 Vue.use(VueMeta);
-Vue.use(moment);
 Vue.use(dayjs);
 
 new Vue({
