e72f9342a7adf84a3735d691d27bdbd150ac4ee3
Yashu Mittal <mittalyashu77@gmail.com>
2021-05-22 07:32:58 +0530

    import filters & styles to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
 import { addDecorator } from '@storybook/vue';
 import centered from '@storybook/addon-centered/vue';
 
+import "../src/filters";
+import "../src/styles/main.sass";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
