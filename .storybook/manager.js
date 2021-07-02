04c6403787b75c7669588a86700805172606e3ab
Yashu Mittal <mittalyashu77@gmail.com>
2021-07-02 20:21:34 +0530

    change storybook theme to light

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,7 +2,7 @@ import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 
 addons.setConfig({
-  theme: themes.dark,
+  theme: themes.light,
   brandTitle: 'LogChimp',
   brandUrl: 'https://logchimp.codecarrot.net/',
   brandImage: 'https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png',
